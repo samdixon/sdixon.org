8fe3605a71d039f8fa9e91684679e5e5f4c98852
samdixon <sdixon@posteo.net>
2022-06-21 21:01:41 -0400

    wip

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+const { cwd, stdout, copy } = Deno;
+import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
+import { renderFileToString } from 'https://deno.land/x/dejs/mod.ts';
 
-serve((_req) => {
-  return new Response("Hello 2", {
-    headers: { "content-type": "text/plain" },
-  });
+const templateDir = "templates";
+
+async function renderTemplateSync(path) {
+  return new Response(await renderFileToString(`${templateDir}/${path}`), {
+    headers: {
+      'content-type': 'text/html'
+    }
+  })
+}
+
+serve({
+  "/": () => renderTemplateSync('index.ejs'),
+  "/projects": () => renderTemplateSync('projects.ejs'),
+  "/about": () => renderTemplateSync('about.ejs'),
+  "/blog": () => renderTemplateSync('blog.ejs'),
+  "/blog/:slug": (_req, _conn, params) => {
+    const post = `Hello, you visited ${params.slug}!`;
+    return new Response(post);
+  },
+  // The route handler of 404 will be invoked when a route handler
+  // for the requested path is not found.
+  404: () => new Response("not found"),
 });
