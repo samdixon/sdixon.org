ec5afa54eec9740e761e26077b1b50ee79733049
samdixon <sdixon@posteo.net>
2022-06-21 22:20:05 -0400

    switch render engine

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const { cwd, stdout, copy } = Deno;
 import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
-import { renderToString } from 'https://deno.land/x/dejs/mod.ts';
+import * as eta from "https://deno.land/x/eta@v1.12.3/mod.ts"
 
-const templateDir = "templates";
+const templateDir = `${Deno.cwd()}/templates`;
 
 async function renderTemplateSync(path) {
-  let f = await Deno.readFile(`${templateDir}/${path}`)
-  return new Response(await renderToString(f), {
+  console.log(r)
+  return new Response(await eta.renderFile(`${templateDir}/${path}`), {
     headers: {
       'content-type': 'text/html'
     }
