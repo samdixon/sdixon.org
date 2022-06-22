0533c8b1ad5beb16271cc41e0fb5bf293be17b37
samdixon <sdixon@posteo.net>
2022-06-21 20:14:30 -0400

    wip

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 serve((_req) => {
-  return new Response("Hello World!", {
+  return new Response("Hello 2", {
     headers: { "content-type": "text/plain" },
   });
 });
